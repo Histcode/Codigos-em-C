efab80b8fdecd715f074d9525916d9e04babe506
Histcode <126244555+Histcode@users.noreply.github.com>
2023-03-08 09:47:15 -0300

    Create Hello World.c

---
+++ Hello World.c
@@ -0,0 +1,6 @@
+#include <stdio.h>     // Seleciona a biblioteca
+
+int main()             //A função main deve ser do tipo int, e nela é retornado 0 para indicar para o sistema que o programa foi encerrado corretamente
+{
+   printf("Hello world");    //Printf é usado para escrever o que esta entre as ""
+}
